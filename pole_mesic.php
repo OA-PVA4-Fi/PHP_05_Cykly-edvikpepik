9e8f786006d8a3b1fb021d9ff8a7e57264b43419
edvikpepik <tomas.both@icloud.com>
2024-10-31 15:26:21 +0100

    eh,

--- pole_mesic.php
+++ pole_mesic.php
@@ -7,6 +7,29 @@
 */
 
 //reseni
+$months = [
+    1 => "Leden",
+    2 => "Únor",
+    3 => "Březen",
+    4 => "Duben",
+    5 => "Květen",
+    6 => "Červen", 
+    7 => "Červenec", 
+    8 => "Srpen", 
+    9 => "Září",
+    10 => "Říjen", 
+    11 => "Listopad", 
+    12 => "Prosinec"
+];
+
+foreach ($months as $index => $month) {
+    echo "$index: $month<br>";
+}
+
+for ($i = 6; $i <= 12; $i++) {
+    echo "$i: " . $months[$i] . "<br>";
+}
+
 
 ?>
 
